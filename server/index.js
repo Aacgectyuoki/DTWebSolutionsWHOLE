a3fe0d8d14685646b7dd942690b5a167c847177b
Shahbaz Khan <shahbazkhaniq@gmail.com>
2023-10-13 22:43:55 +0500

    implemented email template

--- server/index.js
+++ server/index.js
@@ -1,55 +1,53 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const fs = require("fs");
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "5mb" }));
-app.use(express.urlencoded({ extended: true }));
 
-app.use(bodyParser.json());
 const emailConfig = {
-    service: 'Gmail', // Use your email service provider
-    auth: {
-      user: 'shahbazkhaniq@gmail.com',
-      pass: 'bqguitpvcwdvebfx',
-    },
-  };
-  
-  const transporter = nodemailer.createTransport(emailConfig);
-  
-app.use("/api/submit-service", (req,res)=>{
-	
-    try {
-        const formData = req.body;
+  service: 'Gmail', // Use your email service provider
+  auth: {
+    user: 'shahbazkhaniq@gmail.com',
+    pass: 'bqguitpvcwdvebfx',
+  },
+};
 
+const transporter = nodemailer.createTransport(emailConfig);
 
-        const mailOptions = {
-          from: 'maxd4637@gmail.com',
-          to: 'maxd4637@gmail.com', 
-          subject: 'New Service Booking',
-          text: `
-            Service Type: ${formData.serviceType}
-            Date: ${formData.date}
-            Time: ${formData.time}
-          `,
-        };
-      
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error('Error sending email:', error);
-            res.status(500).json({ message: 'Failed to send email' });
-          } else {
-            console.log('Email sent:', info.response);
-            res.status(200).json({ message: 'Email sent successfully' });
-          }
-        });
-    } catch (error) {
-        onsole.log(`Getting error while sending email`, error);
-    }
+const emailTemplate = fs.readFileSync("emailTemplate.html", "utf-8");
 
+app.post("/api/submit-service", (req, res) => {
+  try {
+    const formData = req.body;
 
+    const emailContent = emailTemplate
+      .replace("{{ serviceType }}", formData.serviceType)
+      .replace("{{ date }}", formData.date)
+      .replace("{{ time }}", formData.time);
+
+    const mailOptions = {
+      from: 'shahbazkhaniq@gmail.com',
+      to: 'shahbazaliiq@gmail.com',
+      subject: 'New Service Booking',
+      html: emailContent,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+        res.status(500).json({ message: 'Failed to send email' });
+      } else {
+        console.log('Email sent:', info.response);
+        res.status(200).json({ message: 'Email sent successfully' });
+      }
+    });
+  } catch (error) {
+    console.log('Error sending email:', error);
+    res.status(500).json({ message: 'Failed to send email' });
+  }
 });
 
 app.get("*", (req, res) => {
@@ -57,7 +55,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log(
-    `Listening on port  http://localhost:4000`
-  );
+  console.log(`Listening on port http://localhost:4000`);
 });
