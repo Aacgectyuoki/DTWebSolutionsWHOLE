1bd155a335357f82db03a20ee5926d19d40303da
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2023-11-29 09:43:57 -0500

    netlify

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -8,7 +8,7 @@ import { environment } from 'src/environments/environment';
 })
 export class ApiService {
   // private apiUrl: string = `${environment.apiBaseUrl}/api/submit-service`;
-  private apiUrl: string = `${environment.apiBaseUrl}/.netlify/functions/submit-service`;
+  private apiUrl: string = `${environment.apiBaseUrl}/submit-service`;
 
   constructor(private http: HttpClient) {}
 
