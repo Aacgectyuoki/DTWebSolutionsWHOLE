9251175a205048e03645d776a46983089f9771dd
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2024-01-04 12:58:43 -0500

    1-4-24

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@
 
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://dtwebsolutions.org/.netlify/functions',
+  apiBaseUrl: 'https://dtwebsolutions.org/',
   // apiBaseUrl: '/.netlify/functions',
   // apiBaseUrl: process.env['API_BASE_URL'] || '/.netlify/functions',
 };
